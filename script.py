101d8c8cb798206d874b0dd8445377e618f8fd10
srijahn <srijahnavi.chinthalapudi@iitgn.ac.in>
2025-01-16 00:09:05 +0530

    Fixed pylint errors in script.py

--- script.py
+++ script.py
@@ -1,39 +1,46 @@
+"""
+This module implements the merge sort algorithm and demonstrates its usage.
+"""
+
 def merge(arr, left, mid, right):
+    """
+    Merge two halves of an array in sorted order.
+    """
     n1 = mid - left + 1
     n2 = right - mid
 
-    L = [0] * n1
-    R = [0] * n2
-
+    left_half = [0] * n1
+    right_half = [0] * n2
     for i in range(n1):
-        L[i] = arr[left + i]
+        left_half[i] = arr[left + i]
     for j in range(n2):
-        R[j] = arr[mid + 1 + j]
-
-    i = 0  
-    j = 0 
-    k = left  
-
+        right_half[j] = arr[mid + 1 + j]
+    i = 0
+    j = 0
+    k = left
     while i < n1 and j < n2:
-        if L[i] <= R[j]:
-            arr[k] = L[i]
+        if left_half[i] <= right_half[j]:
+            arr[k] = left_half[i]
             i += 1
         else:
-            arr[k] = R[j]
+            arr[k] = right_half[j]
             j += 1
         k += 1
 
     while i < n1:
-        arr[k] = L[i]
+        arr[k] = left_half[i]
         i += 1
         k += 1
 
     while j < n2:
-        arr[k] = R[j]
+        arr[k] = right_half[j]
         j += 1
         k += 1
 
 def merge_sort(arr, left, right):
+    """
+    Perform merge sort on the array.
+    """
     if left < right:
         mid = (left + right) // 2
 
@@ -42,10 +49,15 @@ def merge_sort(arr, left, right):
         merge(arr, left, mid, right)
 
 def print_list(arr):
+    """
+    Print elements of the array.
+    """
     for i in arr:
         print(i, end=" ")
     print()
 
-arr = [12, 81, 33, 55, 6, 71]
-merge_sort(arr, 0, len(arr) - 1)
-print_list(arr)
+# Test the merge_sort function
+if __name__ == "__main__":
+    array = [12, 81, 33, 55, 6, 71]
+    merge_sort(array, 0, len(array) - 1)
+    print_list(array)
